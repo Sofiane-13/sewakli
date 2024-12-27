9dfad42b02f1f54484f1bd15311a0e51602bd43b
BELHADJ KACEM <sofiane_belhadj_kacem@outlook.fr>
2024-12-27 17:06:22 +0100

    :lock: Add hlmet

--- packages/apps/api/src/main.ts
+++ packages/apps/api/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  )
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
